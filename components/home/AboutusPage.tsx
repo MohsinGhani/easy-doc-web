83f7242c7b0c4a0db52fa20b65dcbc2936cac237
nimra-al <nimrakhan098ali@gmail.com>
2024-09-24 07:31:30 -0700

    working on about section

---
+++ components/home/AboutusPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AboutusPage = () => {
+  return (
+    <div className='h-full border border-black-900 w-full'>
+      <p>hello</p>
+    </div>
+  )
+}
+
+export default AboutusPage
--- components/home/LandingPage.tsx
+++ components/home/LandingPage.tsx
@@ -46,54 +46,111 @@
 // export default LandingPage;
 import React from "react";
 import Image from "next/image";
+import { buttonVariants } from "../ui/button";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
+import AboutusPage from "./AboutusPage";
+// import AboutusPage from "./AboutusPage";
 
 const LandingPage = () => {
   return (
-    <div className="relative h-screen">
-      {/* Left corner image */}
-      <div className="absolute top-0 left-0">
-        <Image
-          src={"/assets/images/landingpage-bg.png"}
-          width={300}
-          height={200}
-          alt="landingpage-bg"
-        />
-      </div>
-
-      {/* Right corner image */}
-      <div className="absolute top-0 right-0">
-        <Image
-          src={"/assets/images/landingpagecircle-bg.png"}
-          width={500}
-          height={400}
-          alt="landingpagecircle-bg"
-        />
-      </div>
-
-      {/* Center bottom image */}
-      <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2">
-        <Image
-          src={"/assets/images/landingpagellipse-bg.png"}
-          width={1000}
-          height={600}
-          alt="landingpagebottom-bg"
-        />
-        
-        {/* Large overlay man image */}
-        <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2  w-[65%]">
-          <Image
-            src={"/assets/images/landingpagemain-img.png"} // Replace with the correct path to your "man" image
-            width={1000} // Larger width
-            height={900} // Larger height
-            alt="man-image"
-          />
-        </div>
-      </div>
+    <>
+    <div className="absolute top-0 left-0 w-full  h-screen">
+  <div className="flex justify-between">
+    <div className="relative z-50 w-80 left-40 hidden md:block">
+      <Image
+        className="w-[100%]"
+        src={"/assets/images/landingpage-bg.png"}
+        width={300}
+        height={200}
+        alt="landingpage-bg"
+      />
     </div>
+    <div className="absolute top-0 right-0 z-50">
+      <Image
+        src={"/assets/images/landingpagecircle-bg.png"}
+        width={500}
+        height={400}
+        alt="landingpagecircle-bg"
+      />
+    </div>
+  </div>
+
+  {/* Pink border div positioned grid jusabove images */}
+  <div className="absolute top-10 md:top-32 left-1/2 z-50 transform -translate-x-1/2 flex flex-col md:flex-row justify-between w-[90%] md:w-[80%] p-4 ">
+  <div className="md:w-1/2 w-full ">
+    <div className="text-[#121212] font-[Outfit] text-[24px] md:text-[54px] font-bold leading-[48px] md:leading-[68.04px] tracking-[0.02em] text-left">
+      Medical Care Now <br/>
+      Simplified For <br/> 
+      <span className="text-[#4D77FF]">Everyone</span>
+    </div>
+
+    <div className="font-[Outfit] text-[14px] md:text-[16px] font-normal leading-[28px] md:leading-[36px] text-left text-[#5C5C5B] mt-4">
+      <p>You Only Have To Know One Thing That You Can <br/> Learn Anything Anywhere To Do You Discover<br/> Yourself.</p>
+    </div>
+
+    <div className="mt-3">
+      <Link
+        href={`/auth/sign-in`}
+        className={cn(
+          buttonVariants({ size: "xl" }),
+          "rounded-full font-bold"
+        )}
+      >
+        Book an appointment            
+      </Link>
+    </div>
+  </div>
+
+  <div className="md:w-96 w-full mt-10 md:mr-12">
+    <div className="w-5 md:w-10 mx-auto mr-96">
+      <Image 
+        className="w-full" 
+        src="/assets/images/landingpageimg.svg" 
+        width={300} 
+        height={200} 
+        alt="landingpage-bg" 
+      />
+    </div>
+
+    <div className="font-[Outfit] text-[16px] md:text-[18px] font-normal leading-[24px] md:leading-[29px] pt-4 text-left text-[#5C5C5B]">
+      <p>You only have to know one thing that you can <br/> learn anything anywhere to do you discover<br/> yourself.</p>
+    </div>
+
+    <div className="flex mt-3">
+      <div className="border-t-2 mt-3 border-[#140F07] w-10"></div>
+      <div className="pl-2 font-[Outfit] text-[#140F07] font-bold text-[14px] md:text-[16px]">Robert Mathew</div>
+    </div>
+  </div>
+</div>
+
+
+ <div className="absolute -bottom-16 left-1/2 transform -translate-x-1/2 w-[60%] hidden md:block">
+ <Image
+    src={"/assets/images/landingpagellipse-bg.png"}
+    width={1000}
+    height={600}
+    alt="landingpagebottom-bg"
+  />
+</div>
+ 
+
+<div className="absolute -bottom-40 left-1/2 transform -translate-x-1/2 -translate-y-1 z-50 w-[56%] hidden md:block">
+  <Image
+    src={"/assets/images/landingpagemain-img.png"}
+    width={1100} 
+    height={900} 
+    alt="man-image"
+  />
+</div>
+
+</div>
+
+  
+    {/* <AboutusPage/> */}
+    </>
   );
 };
 
 export default LandingPage;
-
-
-
